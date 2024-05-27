988d59258417581f3d97da4ade3915764e4860cb
fatma2705 <arafatma2003@gmail.com>
2024-05-27 15:10:52 +0200

    metodo listAll stanza creato e testato correttamente

--- src/main/java/it/prova/prenotazioni/web/api/StanzaController.java
+++ src/main/java/it/prova/prenotazioni/web/api/StanzaController.java
@@ -1,7 +1,10 @@
 package it.prova.prenotazioni.web.api;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +22,12 @@ public class StanzaController {
 
 	@Autowired
 	private StanzaService stanzaService;
-	
+
+	@GetMapping
+	public List<StanzaDTO> listAll() {
+		return StanzaDTO.buildStanzaDTOListFromModelList(stanzaService.listAll(), true);
+	}
+
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public StanzaDTO insertNew(@Valid @RequestBody StanzaDTO input) {
